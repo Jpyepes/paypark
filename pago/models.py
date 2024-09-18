5f8e20213d19f98a7f0baec20a45d1b30ff5e37a
juan <jpyg123@gmail.com>
2024-09-18 18:51:00 -0500

    Add funcionality

--- pago/models.py
+++ pago/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    cedula = models.CharField(max_length=15)
+    saldo = models.FloatField(default=0)
+    fecha_registro = models.DateTimeField(null=True)
